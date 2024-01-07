6cc4d0fd9b618f0295adf819533ae3738cc3ec09
Barbara Ferreira <bbf@charlietango.dk>
2024-01-07 13:04:54 +0100

    fix: type

--- app/components/PageContent/index.tsx
+++ app/components/PageContent/index.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
-export const PageContent = () => {
+export const PageContent = (props: any) => {
 
-  /*const pageText = props.props.content.rendered
+ /* const pageText = props.props.content.rendered
 
-  const pageTitle = props.props.title.rendered*/
+  const pageTitle = props.props.title.rendered
+  */
 
   //   '<p>[vc_row][vc_column][vc_column_text] Om TrylleJan &amp; JuleJan Hvem er TrylleJan &amp; JuleJan? Jeg hedder Jan og er 45 år. Jeg har siden min fars kollega Max tryllede for mig som 6 årig elsket trylleri og tryllet igennem hele mit liv og været fascineret af tryl. Da jeg selv blev far, oplevede jeg en lyst til at trylle for mine børn og til deres mærkedage, så som fødselsdage og skolebegivenheder. Jeg har underholdt børn, voksne og barnlige sjæle med mine tryllekunster, mentalisme og ballonhunde. I 2019 meldte JuleJan sin ankomst og jeg har optrådt som julemand, med tilhørende nisse (Min søn Emil på [&hellip;]</p>\n'
 
