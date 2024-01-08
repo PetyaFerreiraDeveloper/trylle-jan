9294a75e5f81516b58051295cc7f0afc05675125
Barbara Ferreira <bbf@charlietango.dk>
2024-01-08 21:12:44 +0100

    feat: use HtmlText component to render data.content

--- app/components/PageContent/index.tsx
+++ app/components/PageContent/index.tsx
@@ -4,7 +4,7 @@ import { HtmlText } from "@/app/components/HtmlText";
 export const PageContent = ({data}: any) => {
   console.log({data})
 
-  const test = `<p>[vc_row][vc_column][vc_column_text]</p>\n +
+  /*const test = `<p>[vc_row][vc_column][vc_column_text]</p>\n +
     <h1 style="text-align: center;">Om TrylleJan &amp; JuleJan</h1>\n +
     <p style="text-align: center;">Hvem er TrylleJan &amp; JuleJan?</p>\n +
     <p style="text-align: center;">Jeg hedder Jan og er 45 år. Jeg har siden min fars kollega Max tryllede for mig som 6 årig elsket trylleri og tryllet igennem hele mit liv og været fascineret af tryl. Da jeg selv blev far, oplevede jeg en lyst til at trylle for mine børn og til deres mærkedage, så som fødselsdage og skolebegivenheder. Jeg har underholdt børn, voksne og barnlige sjæle med mine tryllekunster, mentalisme og ballonhunde.</p>\n +
@@ -12,7 +12,7 @@ export const PageContent = ({data}: any) => {
     <p style="text-align: center;">Idag, holder jeg især af at trylle for børn, unge og voksne til for eksempel børnefødselsdage, konfirmationer, bryllupper, firmaarrangementer, klubmøder og lign. Så kontakt mig for et godt tilbud til at gøre netop dit arrangement helt fortryllende!</p>\n +
     <p style="text-align: center;">Tøv ikke med at tjekke min Facebook side ud, hvor der er en del mere liv og opdateringer 🙂</p>\n +
     <p>&nbsp;[/vc_column_text][/vc_column][/vc_row][vc_row][vc_column][vc_icon icon_fontawesome=&#8221;fa fa-facebook-official&#8221; size=&#8221;xl&#8221; align=&#8221;center&#8221; link=&#8221;url:http%3A%2F%2Fwww.facebook.com%2FTrylleJan||target:%20_blank|&#8221;][vc_btn title=&#8221;.&#8221; style=&#8221;gradient&#8221; gradient_color_1=&#8221;white&#8221; gradient_color_2=&#8221;white&#8221; link=&#8221;url:http%3A%2F%2Fgg0.us%2Fjang|||&#8221;][/vc_column][/vc_row]</p>`
-
+*/
  /* const pageText = props.props.content.rendered
 
   const pageTitle = props.props.title.rendered
@@ -36,7 +36,7 @@ export const PageContent = ({data}: any) => {
           className="absolute z-10 bottom-0 w-full h-40 flex bg-gradient-to-t from-white to-white/0">
         </div>
       </section>
-      <HtmlText html={test} />
+      <HtmlText html={data.content.rendered} />
       {/*<div className="flex flex-col gap-4 w-full lg:hidden mx-auto px-5 md:px-10 lg:px-20 xl:px-36 py-10 lg:py-0">
         <h1 className="text-3xl">Om TrylleJan & JuleJan</h1>
         <h3>Hvem er TrylleJan & JuleJan?</h3>
