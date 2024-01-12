041fa5d5143b4cb7d0483e247071651fca06ca1c
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-01-12 15:32:33 +0100

    Footer: update footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,10 +2,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className={`flex flex-col justify-center items-center bg-gray-800 text-white mx-auto w-full py-5 px-5 md:px-10 lg:px-20 xl:px-36 text-sm mt-10` }>
-      <p >Contact: info@TrylleJan.dk | Phone: 40 73 01 93</p>
-      <p>&copy; TrylleJan {new Date().getFullYear()}. All rights reserved.</p>
-    </div>
+    <footer
+      className={`flex flex-col gap-5 justify-center items-center bg-gray-800 text-white mx-auto w-full py-5 px-5 md:px-10 lg:px-20 xl:px-36 text-sm mt-10`}
+    >
+      <div className="flex gap-4">
+        <p>
+          <span>Email: </span>
+          <a href="emailto:">info@tryllejan.dk</a>
+        </p>
+        <p>
+          <span>Tlf: </span>
+          <a href="tel">40 73 01 93</a>
+        </p>
+      </div>
+      <p>&copy; TrylleJan {new Date().getFullYear()}. Alle rettigheder forbeholdt.</p>
+    </footer>
   );
 };
 
