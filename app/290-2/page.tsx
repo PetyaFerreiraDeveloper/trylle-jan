b8e4f2a886b5f80e127509d095da687586ff7f25
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-01-12 12:43:36 +0100

    EmilsPage: implement using data from WP

--- app/290-2/page.tsx
+++ app/290-2/page.tsx
@@ -1,23 +1,22 @@
-import { Trade_Winds } from "next/font/google";
-const tradeWinds = Trade_Winds({ weight: "400", subsets: ["latin"] });
+import { FrontPageDataType, GalleryPageType } from "../lib/types";
+import { PageContent } from "../components/PageContent";
+import { getDataWP } from "../api/getDataWP";
 
-const MagicEmilPage = () => {
+const MagicEmilPage = async () => {
+  const data: FrontPageDataType = await getDataWP(
+    "https://tryllejan.dk/wp-json/wp/v2/pages/290",
+    "Failed to get text data",
+  );
+  const imageData: GalleryPageType[] = await getDataWP(
+    "https://tryllejan.dk/wp-json/wp/v2/media?parent=290",
+    "Failed to fetch Hero image",
+  );
+
   return (
-    <section className={"page-section"}>
-      <h1 className={`${tradeWinds.className} mb-4 text-3xl tracking-widest`}>
-        MAGISKE EMIL
-      </h1>
-      <p>
-        Jeg Emil og jeg er 12 år. Da jeg var seks år begyndte jeg at finde
-        interesse for tryl og fik mit første trick og siden da er jeg blevet
-        dygtiger og dygtiger. Som 9 årig begyndt jeg at lave shows med min far
-        og syndes at det var fedt at komme ud at trylle. Nu har jeg selv lyst
-        til at komme ud at trylle for børn og voksende. Jeg vil elske at komme
-        ud til jeres fødselsdag, konfirmation og lign. Så hvis i en dag for lyst
-        til en trylle kunster så hav meg i tankerne jeg vil levere det mes
-        magiske show i nogen sinde kommer til at se.
-      </p>
-    </section>
+    <PageContent
+      text={data.content.rendered}
+      imageUrl={imageData[1]["source_url"]}
+    />
   );
 };
 
