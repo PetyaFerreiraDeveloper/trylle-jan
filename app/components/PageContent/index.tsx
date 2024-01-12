327e920ff3ed8a20741132e16ff30d665e1296a5
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-01-12 10:38:53 +0100

    FrontPage: get hero image from wp

--- app/components/PageContent/index.tsx
+++ app/components/PageContent/index.tsx
@@ -1,24 +1,26 @@
-import Image from "next/image";
-import { HtmlText } from "@/app/components/HtmlText";
+import Image from "next/image"
+import { HtmlText } from "@/app/components/HtmlText"
 
-export const PageContent = ({data}: any) => {
-  const purgedText = data.content.rendered.replace(/\{[^{}]*\}|\([^()]*\)|\[[^[\]]*\]/g,"");
+type Props = {
+  text: string
+  imageUrl: string
+}
+
+export const PageContent = ({ text, imageUrl }: Props) => {
+  const purgedText = text.replace(/\{[^{}]*\}|\([^()]*\)|\[[^[\]]*\]/g, "")
 
   return (
     <>
-      <section
-        className="relative flex w-full min-[1366px]:h-dvh items-start justify-center overflow-hidden flex-col lg:flex-row pt-[88px] md:pt-0 gap-6 px-0">
+      <section className="relative flex w-full min-[1366px]:h-dvh items-start justify-center overflow-hidden flex-col lg:flex-row pt-[88px] md:pt-0 gap-6 px-0">
         <Image
-          src={ '/home-image.jpg' }
-          alt={ 'Hero image' }
-          width={ 0 }
-          height={ 0 }
+          src={imageUrl}
+          alt={"Hero image"}
+          width={0}
+          height={0}
           sizes="100vw"
-          style={ {width: '100%', height: 'auto'} }
+          style={{ width: "100%", height: "auto" }}
         />
-        <div
-          className="absolute z-10 bottom-0 w-full h-40 flex bg-gradient-to-t from-white to-white/0">
-        </div>
+        <div className="absolute z-10 bottom-0 w-full h-40 flex bg-gradient-to-t from-white to-white/0"></div>
       </section>
       <HtmlText html={purgedText} />
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { PageContent } from "./components/PageContent"
+import { FrontPageDataType, GalleryPageType } from "./lib/types"
 
 const getData = async () => {
   const res = await fetch("https://tryllejan.dk/wp-json/wp/v2/pages/58")
@@ -9,12 +10,27 @@ const getData = async () => {
   return res.json()
 }
 
-const Home = async () => {
-  // data is the content on the front page of the wordpress in markup format
-  const data = await getData()
-  console.log({data});
+const getFrontPageImage = async () => {
+  const res = await fetch("https://tryllejan.dk/wp-json/wp/v2/media?parent=58")
+  if (!res.ok) {
+    throw new Error("Failed to fetch FrontPage image")
+  }
+  return res.json()
+}
 
-  return <PageContent data={data} />
+const Home = async () => {
+  // data is the content on the front page of the wordpress in json format
+  const data: FrontPageDataType = await getData()
+
+  // imageData is the content in the wp_attachment object on the front page of the wordpress in json format. it comes as an array with properties for the images on the page
+  const imageData: GalleryPageType[] = await getFrontPageImage()
+
+  return (
+    <PageContent
+      text={data.content.rendered}
+      imageUrl={imageData[0]["source_url"]}
+    />
+  )
 }
 
 export default Home
