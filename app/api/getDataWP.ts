ff4fb0639bb1479c8e031942f64fbb252519db72
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-01-14 17:23:23 +0100

    fetchData: try with cache revalidate 3600

--- app/api/getDataWP.ts
+++ app/api/getDataWP.ts
@@ -1,6 +1,8 @@
 export const getDataWP = async (url: string, errorMsg: string) => {
-  // const res = await fetch(url, { next: { revalidate: 3600 } });
-  const res = await fetch(url, { cache: "no-store" });
+  const res = await fetch(url, { next: { revalidate: 3600 } });
+
+  // this line of code will not cache any of the data
+  // const res = await fetch(url, { cache: "no-store" });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error(errorMsg);
