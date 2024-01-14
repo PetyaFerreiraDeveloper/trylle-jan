be9687a621588c6051a883e980685b83f693fef5
Barbara Ferreira <bbf@charlietango.dk>
2024-01-14 20:24:50 +0100

    feat: add trustpilot widget to frontpage

--- app/components/PageContent/index.tsx
+++ app/components/PageContent/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { HtmlText } from "@/app/components/HtmlText";
-import { DiVim } from "react-icons/di";
 
 type Props = {
   text: string;
@@ -24,6 +23,20 @@ export const PageContent = ({ text, imageUrl, secretBtn }: Props) => {
           priority={true}
         />
         <div className="absolute z-10 bottom-0 w-full h-40 flex bg-gradient-to-t from-white to-white/0"></div>
+        <div className="absolute z-10 top-32 right-0 w-fit h-fit flex">
+          {/* -- TrustBox widget - Review Collector -- */}
+          <div
+            className="trustpilot-widget p-2"
+            data-locale="da-DK"
+            data-template-id="56278e9abfbbba0bdcd568bc"
+            data-businessunit-id="62f2414a683a1b737f983dd9"
+            data-style-height="52px"
+            data-style-width="100%"
+          >
+            <a href="https://dk.trustpilot.com/review/tryllejan.dk" target="_blank" rel="noopener">Trustpilot</a>
+          </div>
+          {/* -- End TrustBox widget -- */}
+        </div>
       </section>
       <section className="flex flex-col px-5 md:px-10 lg:px-20 xl:px-36 lg:text-xl">
         <HtmlText html={purgedText} />
