c99c17d08e4f82183d5497006e4cd54c09df4d88
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-01-14 12:08:01 +0100

    getData: add cache: 'no-store' - not to cache the data
    I could see that after updating content on WP, the nextjs content did not get updated

--- app/api/getDataWP.ts
+++ app/api/getDataWP.ts
@@ -1,5 +1,6 @@
 export const getDataWP = async (url: string, errorMsg: string) => {
-  const res = await fetch(url);
+  // const res = await fetch(url, { next: { revalidate: 3600 } });
+  const res = await fetch(url, { cache: "no-store" });
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error(errorMsg);
