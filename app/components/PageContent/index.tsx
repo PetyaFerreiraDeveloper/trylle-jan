a426467c5a0c8fb945c4cde52925980d2f985802
Barbara Ferreira <bbf@charlietango.dk>
2024-01-14 20:37:50 +0100

    refactor: remove padding of trustpilot widget

--- app/components/PageContent/index.tsx
+++ app/components/PageContent/index.tsx
@@ -26,7 +26,7 @@ export const PageContent = ({ text, imageUrl, secretBtn }: Props) => {
         <div className="absolute z-10 top-32 right-0 w-fit h-fit flex">
           {/* -- TrustBox widget - Review Collector -- */}
           <div
-            className="trustpilot-widget p-2"
+            className="trustpilot-widget"
             data-locale="da-DK"
             data-template-id="56278e9abfbbba0bdcd568bc"
             data-businessunit-id="62f2414a683a1b737f983dd9"
