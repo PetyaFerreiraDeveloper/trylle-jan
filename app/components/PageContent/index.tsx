5e42146b5ff6cfc04ea60eadecb962a1154d7b95
Petya Naydenova Ferreira <pn@abtion.com>
2024-01-15 08:34:47 +0100

    PageContent-decrease padding top

--- app/components/PageContent/index.tsx
+++ app/components/PageContent/index.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import { HtmlText } from "@/app/components/HtmlText";
+import TrustpilotWidget from "../Trustpilot";
 
 type Props = {
   text: string;
@@ -12,7 +14,7 @@ export const PageContent = ({ text, imageUrl, secretBtn }: Props) => {
 
   return (
     <>
-      <section className="relative flex w-full min-[1366px]:h-dvh items-start justify-center overflow-hidden flex-col lg:flex-row pt-[88px] md:pt-0 gap-6 px-0">
+      <section className="relative flex w-full min-[1366px]:h-dvh items-start justify-center overflow-hidden flex-col lg:flex-row pt-[80px] md:pt-0 gap-6 px-0">
         <Image
           src={imageUrl}
           alt={"Hero image"}
@@ -23,20 +25,8 @@ export const PageContent = ({ text, imageUrl, secretBtn }: Props) => {
           priority={true}
         />
         <div className="absolute z-10 bottom-0 w-full h-40 flex bg-gradient-to-t from-white to-white/0"></div>
-        <div className="absolute z-10 top-32 right-0 w-fit h-fit flex">
-          {/* -- TrustBox widget - Review Collector -- */}
-          <div
-            className="trustpilot-widget"
-            data-locale="da-DK"
-            data-template-id="56278e9abfbbba0bdcd568bc"
-            data-businessunit-id="62f2414a683a1b737f983dd9"
-            data-style-height="52px"
-            data-style-width="100%"
-          >
-            <a href="https://dk.trustpilot.com/review/tryllejan.dk" target="_blank" rel="noopener">Trustpilot</a>
-          </div>
-          {/* -- End TrustBox widget -- */}
-        </div>
+
+        {/* <TrustpilotWidget /> */}
       </section>
       <section className="flex flex-col px-5 md:px-10 lg:px-20 xl:px-36 lg:text-xl">
         <HtmlText html={purgedText} />
