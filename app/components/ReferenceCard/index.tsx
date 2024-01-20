61bfd4148bea036baa0a7d42b77a4ee47c33035b
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-01-20 22:54:28 +0100

    referenceCard:format the file

--- app/components/ReferenceCard/index.tsx
+++ app/components/ReferenceCard/index.tsx
@@ -6,15 +6,17 @@ type Props = {
 export const ReferenceCard = (props: Props) => {
   const { title, content, author, position, referenceDate } = props.review;
   return (
-    <article className="flex flex-col gap-2 w-full md:min-w-72 md:max-w-96 items-start">
+    <article className="flex flex-col gap-2 w-full md:min-w-72 md:max-w-96 items-start ">
       <div className="w-full h-[1px] bg-gray-800 my-5"></div>
-      {referenceDate ? <p className="self-end">
-        { referenceDate.toLocaleString("da-DK", {
-          year: "numeric",
-          month: "long",
-          day: "numeric"
-        }) }
-      </p> : null}
+      {referenceDate ? (
+        <p className="self-end">
+          {referenceDate.toLocaleString("da-DK", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+      ) : null}
       <p className="">{content}</p>
       <h4 className="text-xl mt-5">{title}</h4>
       <div className="flex flex-col self-end text-right">
