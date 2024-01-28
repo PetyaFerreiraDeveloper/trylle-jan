16c2e9b4ddf3346ec9902de5e638cf68c0d0192b
Barbara Ferreira <bbf@charlietango.dk>
2024-01-28 20:14:14 +0100

    refactor: fetch magic emil page to fetch data from word press again

--- app/290-2/page.tsx
+++ app/290-2/page.tsx
@@ -1,25 +1,24 @@
 import { FrontPageDataType, GalleryPageType } from "../lib/types";
 import { PageContent } from "../components/PageContent";
 import { getDataWP } from "../api/getDataWP";
-import { textEmilPage } from "../lib/const";
 
 const MagicEmilPage = async () => {
-  // const pageData: FrontPageDataType = await getDataWP(
-  //   "https://tryllejan.dk/wp-json/wp/v2/pages/290",
-  //   "Failed to get text data",
-  // );
+  const pageData: FrontPageDataType = await getDataWP(
+    "https://wp.tryllejan.dk/wp-json/wp/v2/pages/290",
+    "Failed to get text data",
+  );
 
-  // const imageData: GalleryPageType[] = await getDataWP(
-  //   "https://tryllejan.dk/wp-json/wp/v2/media?parent=290",
-  //   "Failed to fetch Hero image",
-  // );
-  let imageUrl: string = "/emil.jpg";
-  // if (imageData.length !== 0) {
-  //   imageUrl = imageData[0]["source_url"];
-  // }
+  const imageData: GalleryPageType[] = await getDataWP(
+    "https://wp.tryllejan.dk/wp-json/wp/v2/media?parent=290",
+    "Failed to fetch Hero image",
+  );
 
-  // return <PageContent text={pageData.content.rendered} imageUrl={imageUrl} />;
-  return <PageContent text={textEmilPage} imageUrl={imageUrl} />;
+  return (
+    <PageContent
+      text={pageData.content.rendered}
+      imageUrl={imageData[0]["source_url"]}
+    />
+  );
 };
 
 export default MagicEmilPage;
